2407a108c52cd3a3a3b335d80530e1a7473373c0
Adamya Gupta <adamyagupta005@gmail.com>
2026-07-22 23:30:03 +0530

    Added new Project - ChatApp

--- Portfolio/src/data/projects.js
+++ Portfolio/src/data/projects.js
@@ -1,9 +1,19 @@
 import fitbalance from '../assets/projectImg/FitBalance.png'
 import mernauth from '../assets/projectImg/mernAuth.png'
+import chatApp from '../assets/projectImg/chatApp.png'
 
 export const projects = [
     {
-        id:1,
+        id: 2,
+        title: 'ChatApp',
+        image: chatApp,
+        description: 'Developed a full-stack real-time chat application supporting group chats, private password-protected rooms, direct messaging, live online/offline presence, and one-to-one video calling using Socket.IO and WebRTC with secure JWT authentication.',
+        techs: ["MERN", "Socket.IO", "WebRTC", "JWT", "MongoDB", "Axios"],
+        github: "https://github.com/Adamya-Gupta-2005/ChatApp",
+        live: "https://chat-app-phi-ten-ga4z8vshkm.vercel.app/"
+    },
+    {
+        id: 1,
         title: 'FitBalance',
         image: fitbalance,
         description: 'Built a full-stack AI-powered fitness tracker that enables users to monitor nutrition and workouts, analyze food images using Google Gemini AI, visualize weekly progress, and securely manage personalized fitness goals through JWT authentication.',
@@ -12,7 +22,7 @@ export const projects = [
         live: "https://fit-balance-ai-fitness-tracker.vercel.app/"
     },
     {
-        id:0,
+        id: 0,
         title: 'Authentication System',
         image: mernauth,
         description: 'Developed a secure MERN authentication system featuring user registration, login, email verification via OTP, password reset, and JWT-based authentication with SMTP email delivery using Nodemailer.',
